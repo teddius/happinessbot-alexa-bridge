98fa25009ff9cd3de75295b94446886daae6351a
Markus Petritz <petritzdesigns@gmail.com>
2017-04-30 13:01:07 +0200

    * init checkin

---
+++ server.js
@@ -0,0 +1,8 @@
+var bridge = require("./lib/alexaBridge.js");
+
+function main() {
+  console.log('Starting...');
+  bridge.start();
+}
+
+main();
---
+++ test/curl.sh
@@ -0,0 +1,1 @@
+curl -vv -H "Content-Type: application/json" -X POST --data "@request.json" "https://alexa-bridge.azurewebsites.net/messages"
